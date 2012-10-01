b39ca60e2f8114081c51ea021ba850ef1ce5557c
Matt Carbone <matt.carbone@gmail.com>
2012-10-01 11:21:28 -0400

    add a connections hash to Pusher

--- lib/grocer/pusher.rb
+++ lib/grocer/pusher.rb
@@ -1,11 +1,18 @@
 module Grocer
   class Pusher
+
+    @@connections = {}
+
     def initialize(connection)
       @connection = connection
     end
 
     def push(notification)
       @connection.write(notification.to_bytes)
+    end
+
+    def self.connections
+      @@connections 
     end
   end
 end
